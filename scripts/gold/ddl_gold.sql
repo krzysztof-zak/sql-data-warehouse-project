4a802b8e7101f3340bbf12f1bd68623429ed03e0
Krzysiuco <121493519+Krzysiuco@users.noreply.github.com>
2026-05-16 07:31:28 +0200

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,94 @@
+/*
+================================================================================
+DDL Script: Creating gols views
+================================================================================
 
+This script creates gold layer views.
+It takes silver layer tables and combines their contents into dimentions and facts
+of the star schema.
+
+This can be used to do analysis and reports.
+================================================================================
+*/
+
+-- =============================================================================
+--Create dim: gold.dim_customers
+-- =============================================================================
+
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
+        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
+    END AS gender,
+    ca.bdate AS birthdate,
+    ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
+GO
+
+-- =============================================================================
+--Create dim: gold.dim_customers
+-- =============================================================================
+
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+CREATE VIEW gold.dim_products AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY cpi.prd_start_dt, cpi.prd_id) AS product_key,
+	cpi.prd_id AS product_id,
+	cpi.prd_key AS product_number,
+	cpi.prd_nm AS product_name,
+	cpi.cat_id AS category_id,
+	pcg.cat AS category,
+	pcg.subcat AS subcategory,
+	pcg.maintenance AS maintenance,
+	cpi.prd_cost AS product_cost,
+	cpi.prd_line AS product_line,
+	cpi.prd_start_dt AS start_date
+FROM silver.crm_prd_info cpi
+LEFT JOIN silver.erp_px_cat_g1v2 pcg
+  ON cpi.cat_id = pcg.id
+WHERE prd_end_dt IS NULL; --Filtering out historical data
+GO
+
+-- =============================================================================
+--Create fac: gold.fact_sales
+-- =============================================================================
+
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS	
+	SELECT
+		sd.sls_ord_num AS order_number,
+		dp.product_key AS product_key,
+		dc.customer_id AS customer_id,
+		sd.sls_order_dt AS order_date,
+		sd.sls_ship_dt AS shipping_date,
+		sd.sls_due_dt AS due_date,
+		sd.sls_sales AS sales_amount,
+		sd.sls_price AS price,
+		sd.sls_quantity AS quantity
+	FROM silver.crm_sales_details sd
+	LEFT JOIN gold.dim_products dp
+	  ON sd.sls_prd_key = dp.product_number
+	LEFT JOIN gold.dim_customers dc
+	ON sd.sls_cust_id = dc.customer_id
+GO
